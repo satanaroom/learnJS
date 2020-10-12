ef129d143b1b2f251da01bc2babb38a6f36fded0
Yan Karpov <oficialbalunov@gmail.com>
2020-10-12 21:12:02 +0800

    Исправил первый урок

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
-let money = 55000;
-let income = "freelance";
-let addExpenses = "FOOD, INTERNET, entertainment";
-let deposit = true;
+let money;
+let income;
+let addExpenses;
+let deposit;
 const mission = 150000;
-let period = 6;
+let period;
 
-console.log(money, income, deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать ' + mission + ' рублей.');
-console.log(addExpenses.toLowerCase().split(', '));
+alert('こんばんは！');
+console.log('Проверка первого урока');
 
-let budgetDay = money / 30;
-console.log(budgetDay);
