a6ebb4cb1345c71550a5815f8d76f9355a96c87f
Yan Karpov <oficialbalunov@gmail.com>
2020-10-14 13:03:56 +0800

    Выполнил четвертое уложненное задание

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const getOneArg = function(oneArg, callback) {
+  if (isNaN(oneArg)) {
+    return oneArg.substr(0, 30) + '...';
+  }else {
+    alert('Передана НЕ строка!');
+  }
+  
+};
+
+console.log(getOneArg(prompt('Введите текст для проверки').trim()));
