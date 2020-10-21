490e0f150ad85312a95c0ddc8ca9316aa3e20042
Yan Karpov <oficialbalunov@gmail.com>
2020-10-21 22:27:41 +0800

    Выполнил задание девятого урока

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,81 @@
 'use strict';
+
+let calculation = document.getElementById('start');
+
+console.log(calculation);
+
+let buttonPlus1 = document.getElementsByTagName('button')[0];
+
+console.log(buttonPlus1);
+
+let buttonPlus2 = document.getElementsByTagName('button')[1];
+
+console.log(buttonPlus2);
+
+let checkbox = document.querySelector('#deposit-check');
+
+console.log(checkbox);
+
+let additionalIncome = document.querySelectorAll('.additional_income-item');
+
+console.log(additionalIncome);
+
+let budgetMonthValue = document.getElementsByClassName('budget_month-value');
+
+console.log(budgetMonthValue);
+
+let budgetDayValue = document.getElementsByClassName('budget_day-value');
+
+console.log(budgetDayValue);
+
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+
+console.log(expensesMonthValue);
+
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+
+console.log(additionalIncomeValue);
+
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+
+console.log(additionalExpensesValue);
+
+let incomePeriodValue = document.getElementsByClassName('income_period-value');
+
+console.log(incomePeriodValue);
+
+let targetMonthValue = document.getElementsByClassName('target_month-value');
+
+console.log(targetMonthValue);
+
+let salaryAmount = document.querySelector('.salary-amount');
+
+console.log(salaryAmount);
+
+let incomeTitle = document.querySelector('.income-title');
+
+console.log(incomeTitle);
+
+let incomeAmount = document.querySelector('.income-amount');
+
+console.log(incomeAmount);
+
+let expensesTitle = document.querySelector('.expenses-title');
+
+console.log(expensesTitle);
+
+let expensesAmount = document.querySelector('.expenses-amount');
+
+console.log(expensesAmount);
+
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+
+console.log(additionalExpensesItem);
+
+let targetAmount = document.querySelector('.target-amount');
+
+console.log(targetAmount);
+
+let periodSelect = document.querySelector('.period-select');
+
+console.log(periodSelect);
