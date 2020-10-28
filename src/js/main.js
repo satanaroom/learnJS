42534fa422258320ecaad713f60aa0eed0c57141
Yan Karpov <oficialbalunov@gmail.com>
2020-10-29 04:08:49 +0800

    Выполнил вторую часть Пятнадцатого урока

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
+
+const helloFirst = new First();
+const helloSecond = new Second();
+
+helloSecond.hello();
