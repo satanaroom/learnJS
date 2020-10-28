0f4b37d21c5415c86a29eb72de2e609429d293d9
Yan Karpov <oficialbalunov@gmail.com>
2020-10-29 00:26:03 +0800

    Выполнил первую часть задания из Четырнадцатого урока. Вторая часть в Тринадцатой ветке

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,64 @@
 'use strict';
+
+let inputText = document.querySelector('input');
+
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.cssText = `
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: ${this.bg};
+      fons-size: ${this.fontSize}px
+    `;
+  }
+  
+  createDiv() {
+    let newDiv = document.createElement('div');
+    document.body.append(newDiv);
+
+    newDiv.style.cssText = this.cssText;
+    newDiv.textContent = this.cssText;
+
+    if(this.selector[0] === '.'){
+      newDiv.classList.add();
+    }
+    newDiv.textContent = "DIV создан!";
+  }
+
+  createP() {
+    let newP = document.createElement('p');
+    document.body.append(newP);
+
+    newP.style.cssText = this.cssText;
+    newP.textContent = this.cssText;
+
+    if(this.selector[0] === '.'){
+      newP.classList.add(this.selector);
+    }
+    newP.textContent = "Параграф создан!";
+  }
+}
+
+let newDiv = new DomElement('.block', 300, 300, 'red', 44);
+let newP = new DomElement('.best', 120, 200, 'blue', 12);
+
+inputText.addEventListener('input', function() {
+  if(inputText.value === '.') {
+    newDiv.createDiv();
+  } else if(inputText.value === '#') {
+    newP.createP();
+  } else if (inputText.value === '') {
+    document.querySelector('div').remove();
+  } 
+});
+
+inputText.addEventListener('input', function() {
+  if (inputText.value === '') {
+    document.querySelector('p').remove();
+  }
+});
