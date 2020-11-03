5ff58eea0028fe6294686d6960a117695d7b437e
Yan Karpov <oficialbalunov@gmail.com>
2020-11-03 23:36:55 +0800

    Исправил Восемнадцатое задание

--- script/script.js
+++ script/script.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', () => {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
-
         function getTimeRemaining() {
             const dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
@@ -24,22 +23,19 @@ window.addEventListener('DOMContentLoaded', () => {
             }
             return { timeRemaining, hours, minutes, seconds };
         }
-        function updateClock() {
+        const updateClock = function() {
             const timer = getTimeRemaining();
-            let idInterval;
             timerHours.textContent = timer.hours;
             timerMinutes.textContent = timer.minutes;
             timerSeconds.textContent = timer.seconds;
-            if (timer.timeRemaining > 0) {
-                idInterval = setInterval(updateClock, 1000);
-            } else {
+            if (timer.timeRemaining < 0) {
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
-                clearInterval(idInterval);
+                clearInterval(updateClock);
             }
-        }
-        updateClock();
+        };
+        setInterval(updateClock, 1000);
     }
     countTimer('04 november 2020');
 });
