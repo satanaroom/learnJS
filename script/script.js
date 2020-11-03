105d23f1fb0b1fadb24da2615bc0e5b95c4f0c0c
Yan Karpov <oficialbalunov@gmail.com>
2020-11-03 17:11:01 +0800

    Добавил базовую структуру из Восемнадцатого урока

---
+++ script/script.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    //Timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000;
+            let seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+            return { timeRemaining, hours, minutes, seconds };
+        }
+        function updateClock() {
+            const timer = getTimeRemaining();
+            let idInterval;
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+            if (timer.timeRemaining > 0) {
+                idInterval = setInterval(updateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                clearInterval(idInterval);
+            }
+        }
+        updateClock();
+    }
+    countTimer('04 november 2020');
+});
