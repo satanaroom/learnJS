261f59ce8e5157653f4a3e7f58358abb6ef55285
Karpov Yan <oficialbalunov@gmail.com>
2020-11-17 17:03:09 +0800

    Выполнил усложненное 28

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
+        select.addEventListener('change', () => {
+            const getData = () => {
+                return new Promise((resolve, reject) => {
+                    const request = new XMLHttpRequest();
+                    request.open('GET', './cars.json');
+                    request.setRequestHeader('Content-type', 'application/json');
+                    request.send();
+                    request.addEventListener('readystatechange', () => {
+                        if (request.readyState === 4) {
+                            return;
+                        } else if (request.status === 200) {
+                            const response = JSON.parse(request.responseText);
+                            resolve(response);                   
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            };
+            const outputData = (data) => {
                 data.cars.forEach(item => {
                     if (item.brand === select.value) {
                         const {brand, model, price} = item;
@@ -19,10 +31,11 @@ document.addEventListener('DOMContentLoaded', () => {
                         Цена: ${price}$`;
                     }
                 });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
+            };
+            getData()
+                .then(outputData)
+                .catch(() => {
+                    output.innerHTML = 'Произошла ошибка';
+                });
         });
-    });
-
 });
